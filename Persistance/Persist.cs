5d0d90955b0431d6977ce57fceab6401bb188a8a
nad407 <37850036+nad407@users.noreply.github.com>
2018-05-21 17:14:34 +0200

    CodeWars Kata using C#

---
+++ Persistance/Persist.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersistantBugger
+{
+    class Persist
+    {
+        public static int Persistence(long n)
+        {
+            List<int> numbers = new List<int>();
+            int answer = 1;
+            int mult = 0;
+
+            while (n > 9)
+            {
+                while (n > 0)
+                {
+                    numbers.Add(Convert.ToInt32(n % 10));
+                    n = n / 10;
+                }
+
+                foreach (int number in numbers)
+                {
+                    answer = answer * number;
+                }
+
+                mult++;
+
+                if (answer < 10)
+                {
+                    return mult;
+                }
+                else
+                {
+                    n = answer;
+                    answer = 1;
+                    numbers.Clear();
+                }
+            }
+
+            return 0;
+        }
+    }
+}
+
+        
---
+++ Persistance/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace PersistantBugger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int answer = Persist.Persistence(999);
+            Console.WriteLine(answer);
+            Console.ReadKey();
+        }
+    }
+}
